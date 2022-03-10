0675414bc2989258a8e82ee2c5c454bf28ebef2c
yonghuic <75155638+yonghuic@users.noreply.github.com>
2022-03-10 16:42:39 +0800

    Update week01-4.cpp

--- week01-4.cpp
+++ week01-4.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include<string.h>
 char line[2000];
 int main()
 {
+    for(int k=0;k<4;k++){
     scanf("%s", line);
-    for(int i=0;i<2000;i++)
+    int N = strlen(line);
+    int ans = 0;
+    for(int i=0;i<N;i++)
     {
-        if(line[i] == '2')printf("§ä¨ì2\n");
+        if(line[i] == '2')ans++;
+    }
+    printf("%d\n", ans);
     }
     return 0;
 }
